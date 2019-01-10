3f0c184780917f4e601cb846444e6c7bf477cc18
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-01-10 11:20:29 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'users', component: HomeComponent },
+  { path: '', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
 ];
 
