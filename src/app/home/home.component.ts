e0ba6382655ab737345d08fffd41baa73869fc08
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-01-12 08:16:06 -0500

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
+declare var myAct: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,6 +20,7 @@ export class HomeComponent implements OnInit {
 
 
   ngOnInit() {
+    myAct();
     this.data.getUsers().subscribe(data => {
         this.users = data
         console.log(this.users);
