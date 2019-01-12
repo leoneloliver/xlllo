4e93cf7fef529d9f6cb5783f9ea73213037a8661
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-01-12 08:41:38 -0500

    updates

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,16 +6,15 @@ import { HttpClient } from '@angular/common/http';  // Import it up here
 })
 export class DataService {
 
-	
-
   constructor(private http: HttpClient) {}
 
   getUsers() {
 
-  return this.http.get('https://api.myjson.com/bins/1b7s8m')
+  return this.http.get('https://randomuser.me/api/?results=50')
+	// return this.http.get('https://api.myjson.com/bins/1b7s8m')
+
   //return this.http.get('https://reqres.in/api/users')
   //return this.http.get('https://api.myjson.com/bins/7qasg')
 
-   
   }
 }
