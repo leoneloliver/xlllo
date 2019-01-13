5e18ab784476a675e013d93c1742582210cdf350
Leonel Oliveira <leoneloliviera@Leonels-MacBook-Air.local>
2019-01-13 09:57:33 -0500

    new popup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,6 +24,7 @@ export class HomeComponent implements OnInit {
     this.data.getUsers().subscribe(data => {
         this.users = data
         console.log(this.users);
+        // myAct();
       }
     );
   }
--- src/script.js
+++ src/script.js
@@ -2,4 +2,36 @@
 function myAct(){
 	console.log("loading list of tutors...");
 
+	
+
+	setTimeout(function(){ 
+		var classname = document.getElementsByClassName("pb-upper");
+			var myFunction = function() {
+		    var name = this.getAttribute("data-name");
+		    var email = this.getAttribute("data-email");
+		    var address = this.getAttribute("data-address");
+		    var avatar = this.getAttribute("data-avatar");
+
+		    //alert(name+'-'+email+'-'+address+'-'+avatar);
+		   
+		    document.getElementById("avatar").src=avatar;
+		    document.getElementById("name").innerText=name;
+		    document.getElementById("address").innerText=address;
+		    document.getElementById("email").innerText=email;
+		    document.getElementById("my-modal").classList.add("show-it");   
+		   };
+		  Array.from(classname).forEach(function(element) {
+		    element.addEventListener("click", myFunction);
+		  });
+		  // document.getElementById("close").addEventListener("click", closeModal);
+		  // function closeModal(){
+		  //   document.getElementById("my-modal").classList.remove("show-it");
+		  //   document.getElementById("my-modal").getElementsByTagName("iframe")[0].src="";
+		  // }
+
+
+		}, 1000);
+
+
+
 }
